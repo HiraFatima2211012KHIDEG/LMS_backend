a23a1005091d02b34617f9442874729e14817a2e
HiraArain <hiraarain100@gmail.com>
2024-08-15 09:25:13 +0500

    setting

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -82,31 +82,22 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'HOST': os.getenv('DB_HOST'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         # 'HOST': 'localhost',
-#         # 'NAME': 'test',
-#         # 'USER': 'postgres',
-#         # 'PASSWORD': 'password',
-
-
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'LMS',
-        'USER': 'maazjavaid',
-        'PASSWORD': 'Maaz12..',
+        # 'HOST': os.getenv('DB_HOST'),
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USER'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+
+
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
